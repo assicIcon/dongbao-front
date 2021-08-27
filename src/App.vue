5b8c445056aaf4ba52b6efce332492af5aa7e433
assicIcon <429725883@qq.com>
2021-08-27 17:42:35 +0800

    sider start..

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <sider></sider>
   </div>
 </template>
 
 <script>
+import Sider from '@/components/common/sider/Sider'
 
 export default {
   name: 'App',
+  components: {
+    Sider
+  }
 }
 </script>
 
--- src/components/common/sider/Sider.vue
+++ src/components/common/sider/Sider.vue
@@ -1,13 +1,22 @@
 <template>
-  <div></div>
+  <div class="sider-contain">
+    
+  </div>
 </template>
 
 <script>
 export default {
-  naem: "sider"
+  naem: "Sider"
 }
 </script>
 
 <style>
-
+.sider-contain {
+  width: 210px;
+  height: 100%;
+  overflow: hidden;
+  background-color: #304156;
+  display: flex;
+  height: 937px; /* temp height, slove: height is cover the full screen on y axis  */
+}
 </style>
